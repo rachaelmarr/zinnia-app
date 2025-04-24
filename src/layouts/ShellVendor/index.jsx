6dc3a989000fd291aaeec26ff935aceb28c5e201
Rahul chhabra <chh_rahul@live.com>
2025-04-23 21:31:04 -0500

    Refactor ShellVendor to use VendorHeader and Outlet

--- src/layouts/ShellVendor/index.jsx
+++ src/layouts/ShellVendor/index.jsx
@@ -1,9 +1,16 @@
-import VendorSidebar from '../../components/layouts/VendorSidebar';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import VendorHeader from '../../shared/components/VendorHeader';
+import VendorSidebar from '../VendorSidebar';
+
 
 export default function ShellVendor() {
   return (
     <div className="min-h-screen bg-[#F5F5F5] flex">
+      {/* Collapsible left nav */}
       <VendorSidebar />
+
+      {/* Main content area */}
       <div className="flex-1">
         <VendorHeader isLoggedIn={true} />
         <main className="p-6">
